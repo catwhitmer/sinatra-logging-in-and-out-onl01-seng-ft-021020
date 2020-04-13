c729f3404ab6166341e424a4a32ed760f7f7489e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-04-13 17:04:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
      @user = User.find_by_id(session[:user_id])
-    if @user && is_logged_in?
+    if @user && is_logged_in?(session)
       erb :account
     else
       erb :error
